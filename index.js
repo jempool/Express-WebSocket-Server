a65ae7fe6364c7297d7f08d5ead7429c8efa7937
Jem Suarez <jempoolsmz@gmail.com>
2023-11-13 10:52:57 -0500

    Moving webSocket's logic to a separate module

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { createServer } from "http";
-import { Server } from "socket.io";
 import passport from "passport";
 import cors from "cors";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import * as dbService from "./src/services/db.service.js"; // move
-
+import * as WebSockets from "./src/webSockets/webSockets.js";
 import { PORT, DATABASE_URL, DATABASE_NAME } from "./src/utils/constants.js";
 import "./src/services/auth.service.js";
 import authRouter from "./src/routes/auth.route.js";
 import chatRouter from "./src/routes/chat.route.js";
 
+
 // =================================================================================
-// API Server  =====================================================================
+// API  ============================================================================
 // =================================================================================
 
 const app = express();
@@ -28,33 +27,13 @@ app.use("/chat", passport.authenticate("jwt", { session: false }), chatRouter);
 
 
 // =================================================================================
-// WebSockets  =====================================================================
+// Server  =========================================================================
 // =================================================================================
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
 
-io.on("connection", (socket) => {
-  console.log(`${new Date()} - New connection ${socket.id}`);
-
-  // Listening for chat event
-  socket.on("chat", function (data) {
-    dbService.addMessage(data); // move
-    io.sockets.emit("chat", data);
-  });
-
-  // Listening for typing event
-  socket.on("typing", function (data) {
-    io.sockets.emit("typing", data);
-    socket.broadcast.emit("typing", data);
-  });
-});
-
+// === WebSockets ===
+WebSockets.socketIO(httpServer);
 
 const start = async () => {
   try {
